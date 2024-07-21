3f3e8a2fdcfc4a6048c9c20a72e63139f9edec74
sa1dakhmedovv <159274703+sa1dakhmedovv@users.noreply.github.com>
2024-07-21 16:59:59 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+# print('informatika'[2:5])
+# def f(x):
+#     if x<=3:
+#         return x
+#     return 3*f(x-1)+2*f(x-2)
+# print(f(6))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# a = 0x3a
+# print(a)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def f(x):
+    if x<=3:
+        return x
+    return
